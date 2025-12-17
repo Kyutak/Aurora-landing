5d748d247da9767a1ad6e575ef129b477d62530c
Maressa Palhano <81597611+Kyutak@users.noreply.github.com>
2025-12-17 13:51:09 -0300

    Adjust text size in hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -67,7 +67,7 @@ export function HeroSection() {
         <div className="text-center animate-fade-in-up space-y-0.5 my-[-39px]">
           <h1
             id="hero-heading"
-            className="font-bold tracking-tight text-foreground text-balance text-3xl sm:text-4xl md:text-5xl lg:text-6xl my-5"
+            className="font-bold tracking-tight text-foreground text-balance text-3xl sm:text-5xl md:text-5xl lg:text-6xl my-5"
           >
             Cuidado inteligente para{" "}
             <span className="bg-gradient-to-r from-primary via-accent to-secondary bg-clip-text text-transparent">
@@ -76,7 +76,7 @@ export function HeroSection() {
             </span>
           </h1>
 
-          <p className="text-lg text-muted-foreground text-pretty leading-relaxed sm:text-lg md:text-2xl max-w-2xl mx-auto my-[23px] py-0">
+          <p className="text-sm sm:text-base md:text-lg text-muted-foreground text-pretty leading-relaxed max-w-2xl mx-auto my-[23px] py-0">
             {"Rotinas e funções totalmente personalizáveis. Menos preocupação para você, mais autonomia para o idoso"}
           </p>
 
