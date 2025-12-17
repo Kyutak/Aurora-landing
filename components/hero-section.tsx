da8bcf3fd4cf433f80c55013184caf397e4ac0df
Maressa Palhano <81597611+Kyutak@users.noreply.github.com>
2025-12-17 13:28:13 -0300

    Teste hI
    Updated hero heading font size for better responsiveness.

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -67,7 +67,7 @@ export function HeroSection() {
         <div className="text-center animate-fade-in-up space-y-0.5 my-[-39px]">
           <h1
             id="hero-heading"
-            className="font-bold tracking-tight text-foreground text-balance sm:text-4xl md:text-5xl lg:text-7xl text-5xl my-5"
+            className="font-bold tracking-tight text-foreground text-balance text-3xl sm:text-4xl md:text-5xl lg:text-7xl text-5xl my-5"
           >
             Cuidado inteligente para{" "}
             <span className="bg-gradient-to-r from-primary via-accent to-secondary bg-clip-text text-transparent">
